d90fbc6563c737e0a3db2f489542a54d00e0f362
Robert Fiorentino <rfiorentino@themeetgroup.com>
2024-03-25 16:08:50 -0400

    Add video player to AppContent
    Change-Id: I5e7fed12a188dba7be300f6eb1843a435b89ec43

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export default function App() {
         step: "intro",
         isNavHidden: false,
         image: "https://tapinto-production.s3.amazonaws.com/uploads/articles/im/best_crop_fe6a7fc41e67683a6201_IMG_9780.JPG",
+        videoId: "i3JqgCmjLsk",
         text: "Travel to Evolution Candy and press the OK button to contact the detectives."
     })
 
--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -6,6 +6,19 @@ export default function AppContent(props: { storyStep: StoryStep }) {
 
     return (
         <div className="appContent">
+            {/* Consider moving video into a component */}
+            {props.storyStep.videoId && <iframe
+                style={{
+                    position: 'relative',
+                    width: '100%',
+                    height: '200px',
+                    objectFit: 'cover',
+                    overflow: 'hidden'
+                }}
+            src={`https://www.youtube.com/embed/${props.storyStep.videoId}?autoplay=1&controls=0&enablejsapi=1`}
+            title="YouTube video player"
+            allow="autoplay; clipboard-write; encrypted-media"
+            ></iframe>}
             {props.storyStep.image && <img src={props.storyStep.image} className="appContent--image"/>}
             {props.storyStep.text && <h2>{props.storyStep.text}</h2>}
         </div>
